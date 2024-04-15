d54f05c15781a260aa20ff78ab57edb44c5c7f08
RABL1N <rasmusbernthlinnemann@gmail.com>
2024-04-15 15:09:08 +0200

    gg

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from sympy import *
 init_printing()
 
-def puzzle1():
+def opgave2():
     # Opg 2 og 3
     A = Matrix([[1, 1, 0, 0],[0, 0, 1, 1],[1, 0, 1, 0],[0, 1, 0, 1]])
     b = Matrix([5, 2, 3, 4])
     A,b
     return(linsolve((A,b)))
-print(puzzle1())
-
 
 def puzzle2_figur3():
     # Figur 3
@@ -24,3 +22,8 @@ def puzzle2_figur4():
                 [0,1,0,0,0,1,0,0,0],[1,0,0,0,1,0,0,0,1],[0,0,0,1,0,0,0,1,0]]) # Bund højre fra top til bund
     b = Matrix([5,3,5,2,3,8,2,3,4,3,4,3])
     linsolve((A,b)), A, b
+
+
+if __name__ == "__main__":
+    print("Opgave 2")
+    print(opgave2())
